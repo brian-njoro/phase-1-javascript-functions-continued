f5ff24aa0b4e00800deaa74d15f12ea933ac15a9
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-06-13 19:45:37 +0300

    add respective changes to index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+let activity 
+function saturdayFun(activity = `roller skate`) {
+  return `This Saturday, I want to ${activity}`;
+}
+
+const mondayWork = function(mondayActivity = "go to the office") {
+  return `This Monday, I will ${mondayActivity}.`;
+};
+
+console.log(mondayWork)
+console.log(saturdayFun())
+
+// faulty
+function wrapAdjective () {
+    return function (parameter1 =`special`) {
+return `you are ${parameter1}`
+    }
+}
+
+
+
+
